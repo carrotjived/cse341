6a568e20674ae745a46018b811d8b193c592614b
carrotjived <jakewolfman131@gmail.com>
2024-09-21 16:06:30 +0800

    connected mongodb to node and started the get requests

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("hello World");
-});
+// router.get("/", (req, res) => {
+//   res.send("hello World");
+// });
+
+router.get("/contacts", require("./contacts"));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const mongodb = require("./data/database");
 
 const port = process.env.PORT || 3000;
 
 app.use("/", require("./routes"));
 
-app.listen(port, () => {
-  console.log(`Running in Port: ${port}`);
+mongodb.initDB((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database listening and node Running on port: ${port}`);
+    });
+  }
 });
